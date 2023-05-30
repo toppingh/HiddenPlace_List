ca961795264eb2965d373b0f93911abc0b038e24
topping <reyna11@naver.com>
2023-05-30 20:13:17 +0900

    last commit

--- place/models.py
+++ place/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    openTime = models.IntegerField()
+    closeTime = models.IntegerField()
+    hompage = models.URLField()
+    review = models.FloatField()
+    image = models.ImageField()
+    description = models.TextField()
